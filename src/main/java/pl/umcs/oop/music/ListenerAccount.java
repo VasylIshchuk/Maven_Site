b5cd454b335d57b09956443d4347af9c72433728
VasylIshchuk <Vasyl.Ishchuk@live.umcs.edu.pl>
2024-05-11 17:12:42 +0200

    Task 3(e,d)

--- src/main/java/pl/umcs/oop/music/ListenerAccount.java
+++ src/main/java/pl/umcs/oop/music/ListenerAccount.java
@@ -6,12 +6,14 @@ import pl.umcs.oop.database.DatabaseConnection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Locale;
 
 public class ListenerAccount extends Account {
-    public ListenerAccount(int id, String username, int credits, Playlist playlist) {
+    public ListenerAccount(int id, String username) {
         super(id, username);
     }
+
     public static void init() {
         try {
             String createSQLTable = "CREATE TABLE IF NOT EXISTS accounts( " +
@@ -26,25 +28,25 @@ public class ListenerAccount extends Account {
             throw new RuntimeException(e);
         }
     }
-    public static void addCredit(int id, int credit){
+    public void addCredit( int credit){
         String sql = "UPDATE accounts SET credit=? WHERE id=?";
         try {
            PreparedStatement statement = DatabaseConnection.getConnection()
                    .prepareStatement(sql);
            statement.setInt(1,credit);
-           statement.setInt(2,id);
+           statement.setInt(2,this.id);
            statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static int getCredit(int id){
+    public int getCredit(){
         String sql = "SELECT * FROM accounts WHERE id=?";
         try {
             PreparedStatement statement = DatabaseConnection.getConnection()
                     .prepareStatement(sql);
-            statement.setInt(1,id);
+            statement.setInt(1,this.id);
             ResultSet resultSet = statement.executeQuery();
             return resultSet.getInt(4);
 //            Returns:
@@ -53,5 +55,69 @@ public class ListenerAccount extends Account {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+//    additional method
+    public void addSong(Song song){
+        String sql = "UPDATE accounts SET playlist=? WHERE id=?";
+        try {
+            PreparedStatement statement = DatabaseConnection.getConnection()
+                    .prepareStatement(sql);
+            statement.setString(1,String.format(Locale.ENGLISH,
+                    "{%s,%s,%s} ",song.artist(),song.title(),song.duration()));
+            statement.setInt(2,this.id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+//    additional method
+    public void deleteRecord(){
+        String sql = "DELETE FROM accounts  WHERE id=?";
+        try {
+            PreparedStatement statement = DatabaseConnection.getConnection()
+                    .prepareStatement(sql);
+            statement.setInt(1,this.id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void buySong(int idSong) throws NotEnoughCreditsException {
+        try {
+            Song song = Song.Persistence.read(idSong).get();
+            String sqlAccount = "SELECT * FROM accounts WHERE id=? AND playlist=?";
+            PreparedStatement statementAccount = DatabaseConnection.getConnection()
+                    .prepareStatement(sqlAccount);
+            statementAccount.setInt(1,this.id);
+            statementAccount.setString(2,String.format(Locale.ENGLISH,
+                    "{%s,%s,%s} ",song.artist(),song.title(),song.duration()));
+
+            ResultSet resultSet2 = statementAccount.executeQuery();
+            if(!resultSet2.next()){
+                if(this.getCredit()<=0)
+                    throw new NotEnoughCreditsException("Not enough credits");
+                String sql = "UPDATE accounts SET playlist=? ,credit = credit-1 WHERE id=?";
+
+                PreparedStatement statement = DatabaseConnection.getConnection()
+                        .prepareStatement(sql);
+                statement.setString(1,String.format(Locale.ENGLISH,
+                        "%s,%s,%s",song.artist(),song.title(),song.duration()));
+                statement.setInt(2,this.id);
+                statement.executeUpdate();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Playlist createPlaylist(List<Integer> listIdSongs) throws NotEnoughCreditsException {
+        Playlist playlist = new Playlist();
+        for(Integer idSong: listIdSongs) {
+            this.buySong(idSong);
+             playlist.add(Song.Persistence.read(idSong).get());
+        }
+        return playlist;
     }
 }
--- src/test/java/pl/umcs/oop/music/ListenerAccountTest.java
+++ src/test/java/pl/umcs/oop/music/ListenerAccountTest.java
@@ -8,7 +8,8 @@ import pl.umcs.oop.database.DatabaseConnection;
 
 import javax.naming.AuthenticationException;
 
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +17,7 @@ class ListenerAccountTest {
     @BeforeAll
     public static void connectWithDatabase(){
         DatabaseConnection.connect("songs.db");
+        ListenerAccount.init();
     }
 
     @AfterAll
@@ -24,29 +26,75 @@ class ListenerAccountTest {
     }
     @Test
     public void testRegisterAccount() {
-        ListenerAccount.init();
-        Playlist playlist = new Playlist();
-        Account account = new ListenerAccount(1,"Wild beast",7, playlist);
-        int id = Account.Persistence.register("Wild beast","nj430sp4");
-        assertEquals(id,account.getId());
+        int id = Account.Persistence.register(
+                "Lord universe","5d98gvb9d");
+        Account account = new ListenerAccount(id,"Lord universe");
+        assertEquals(account.getId(),id);
     }
     @Test
     public void testAuthenticateAccount() throws AuthenticationException {
-        Playlist playlist = new Playlist();
-        Account listenerAccount = new ListenerAccount(1,"Wild beast",7, playlist);
-        Account account = Account.Persistence.authenticate("Wild beast","nj430sp4");
-        assertEquals(account.getId(),listenerAccount.getId());
+        Account listenerAccount = new ListenerAccount(1,"Wild beast");
+        Account account = Account.Persistence.authenticate(
+                "Wild beast","nj430sp4");
+        assertEquals(listenerAccount.getId(),account.getId());
     }
     @Test
-    public void testMethodGetCredit(){
-        int credit = ListenerAccount.getCredit(1);
-        assertEquals(credit,0);
+    public void testMethodGetCreditEmpty(){
+        ListenerAccount listenerAccount = new ListenerAccount(
+                3,"Lord universe");
+        int credit = listenerAccount.getCredit();
+        assertEquals(0,credit);
     }
     @Test
     public void testMethodAddCredit(){
-        ListenerAccount.addCredit(1,3);
-        assertEquals(ListenerAccount.getCredit(1),3);
+        ListenerAccount listenerAccount = new ListenerAccount(
+                2,"Classical spice");
+        listenerAccount.addCredit(6);
+        assertEquals(6,listenerAccount.getCredit());
     }
+    @Test
+    public void  testMethodBuySongWhenSongIsInAccount() throws NotEnoughCreditsException {
+        int id  = 9;//Account.Persistence.register("Super Frank", "jks56sbk23l");
+        ListenerAccount listenerAccount = new ListenerAccount(id,"Super Frank");
+        listenerAccount.addCredit(4);
+        Song song = new Song("Elvis Presley","Suspicious Minds",262);
+        listenerAccount.addSong(song);
+        listenerAccount.buySong(9);
+        assertEquals(4,listenerAccount.getCredit());
+    }
+    @Test
+    public void  testMethodBuySongWhenNOSongInAccount() throws NotEnoughCreditsException {
+        int id = Account.Persistence.register("King octopus", "jks56sbk23l");
+        ListenerAccount listenerAccount = new ListenerAccount(id,"King octopus");
+        listenerAccount.addCredit(4);
+        listenerAccount.buySong(9);
+        assertEquals(3,listenerAccount.getCredit());
+    }
+    @Test
+    public void testMethodBuySongWhenNOSongInAccountAndCreditNull(){
+        int id  = Account.Persistence.register("Octavian", "sk23ni492");
+        ListenerAccount listenerAccount = new ListenerAccount(id,"Octavian");
+        assertThrows(NotEnoughCreditsException.class,()->listenerAccount.buySong(9));
+    }
+    @Test
+    public void testCreatePlaylist() throws NotEnoughCreditsException {
+        Song song1 = new Song("James Brown","I Got You (I Feel Good)",167);
+        Song song2 = new Song("The Who","My Generation",198);
+        Song song3 = new Song("The Rolling Stones","Paint It Black",224);
+        Song song4 = new Song("The Beach Boys","Good Vibrations",215);
+        Playlist manuallyPlaylist = new Playlist();
+        manuallyPlaylist.add(song1); manuallyPlaylist.add(song2);
+        manuallyPlaylist.add(song3); manuallyPlaylist.add(song4);
 
+        int id = Account.Persistence.register("Darling", "love42");
+        ListenerAccount listenerAccount = new ListenerAccount(id,"darling");
+        listenerAccount.addCredit(10);
 
+        List<Integer> listIdSongs = new ArrayList<>();
+        listIdSongs.add(39);  listIdSongs.add(10);
+        listIdSongs.add(24);  listIdSongs.add(8);
+
+        Playlist playlist=listenerAccount.createPlaylist(listIdSongs);
+        assertEquals(manuallyPlaylist,playlist);
+    }
 }
